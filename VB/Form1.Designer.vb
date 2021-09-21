7b6a9b9a1dd7f4ac1f0aa0b5d880e49b49c08f24
DevExpressExampleBot <fakeEmail>
2021-09-21 13:21:18 +0300

    autogenerate VB

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,86 +1,85 @@
-﻿Imports DevExpress.XtraCharts
+Imports DevExpress.XtraCharts
 
 Namespace ExportToXLS
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
-			Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
-			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' chartControl1
-			' 
-			Me.chartControl1.Legend.Name = "Default Legend"
-			Me.chartControl1.Location = New System.Drawing.Point(82, 33)
-			Me.chartControl1.Name = "chartControl1"
-			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
-			Me.chartControl1.Size = New System.Drawing.Size(640, 360)
-			Me.chartControl1.TabIndex = 0
-			' 
-			' simpleButton1
-			' 
-			Me.simpleButton1.Location = New System.Drawing.Point(82, 431)
-			Me.simpleButton1.Name = "simpleButton1"
-			Me.simpleButton1.Size = New System.Drawing.Size(75, 23)
-			Me.simpleButton1.TabIndex = 1
-			Me.simpleButton1.Text = "Export to File"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
-			' 
-			' simpleButton2
-			' 
-			Me.simpleButton2.Location = New System.Drawing.Point(255, 431)
-			Me.simpleButton2.Name = "simpleButton2"
-			Me.simpleButton2.Size = New System.Drawing.Size(97, 23)
-			Me.simpleButton2.TabIndex = 2
-			Me.simpleButton2.Text = "Export to Stream"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(821, 483)
-			Me.Controls.Add(Me.simpleButton2)
-			Me.Controls.Add(Me.simpleButton1)
-			Me.Controls.Add(Me.chartControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
-			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.Legend.Name = "Default Legend"
+            Me.chartControl1.Location = New System.Drawing.Point(82, 33)
+            Me.chartControl1.Name = "chartControl1"
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
+            Me.chartControl1.Size = New System.Drawing.Size(640, 360)
+            Me.chartControl1.TabIndex = 0
+            ' 
+            ' simpleButton1
+            ' 
+            Me.simpleButton1.Location = New System.Drawing.Point(82, 431)
+            Me.simpleButton1.Name = "simpleButton1"
+            Me.simpleButton1.Size = New System.Drawing.Size(75, 23)
+            Me.simpleButton1.TabIndex = 1
+            Me.simpleButton1.Text = "Export to File"
+            Me.simpleButton1.Click += New System.EventHandler(AddressOf Me.simpleButton1_Click)
+            ' 
+            ' simpleButton2
+            ' 
+            Me.simpleButton2.Location = New System.Drawing.Point(255, 431)
+            Me.simpleButton2.Name = "simpleButton2"
+            Me.simpleButton2.Size = New System.Drawing.Size(97, 23)
+            Me.simpleButton2.TabIndex = 2
+            Me.simpleButton2.Text = "Export to Stream"
+            Me.simpleButton2.Click += New System.EventHandler(AddressOf Me.simpleButton2_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(821, 483)
+            Me.Controls.Add(Me.simpleButton2)
+            Me.Controls.Add(Me.simpleButton1)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.Load += New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private chartControl1 As ChartControl
-		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
-		Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
-	End Class
+#End Region
+        Private chartControl1 As ChartControl
+
+        Private simpleButton1 As DevExpress.XtraEditors.SimpleButton
+
+        Private simpleButton2 As DevExpress.XtraEditors.SimpleButton
+    End Class
 End Namespace
---
+++ VB/Properties/AssemblyInfo.vb
@@ -0,0 +1,30 @@
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly:AssemblyTitle("BindUsingTemplatesRuntimeCS")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("Дом")>
+<Assembly:AssemblyProduct("BindUsingTemplatesRuntimeCS")>
+<Assembly:AssemblyCopyright("Copyright © Дом 2007")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly:ComVisible(False)>
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly:Guid("6b0e149f-4d4a-4b67-a174-a38f7e175905")>
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
